7acb81becd39800663bf27de2405e75ac1a4eba6
Abdelrahman Mahmoud <74074348+Abd0lRahman@users.noreply.github.com>
2023-10-09 22:12:38 +0300

    Add files via upload

---
+++ 01-MatlabBasics/MatlabFirstDay/Labs/LabPart2.m
@@ -0,0 +1,4 @@
+%part2
+rowVector = [1 2 3 4 5];
+columnVector = [6;7;8;9;10];
+horizontalConcat = horzcat(rowVector,columnVector') %Concatenate two matrices
---
+++ 01-MatlabBasics/MatlabFirstDay/Labs/LabPart3.m
@@ -0,0 +1,3 @@
+%part3
+originalMatrix = [1 2 ;3 4];
+repeatedMatrix = repmat(originalMatrix,2,2) % Repeat matrix
